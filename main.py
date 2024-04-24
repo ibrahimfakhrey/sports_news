bb7e83438a8fb6f0d80c5759cec78881f5596f77
ibrahimfakhry <Ibrahim fakhrey@github.com>
2024-04-24 13:39:40 +0200

    ffff

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import requests
 
 app=Flask(__name__)
 
 END_POINT="https://api.openweathermap.org/data/2.5/weather"
+
+
 API_KEY="6ab73d57dd3ea5445996fd55de54ad44"
-params = {
-    'appid': API_KEY,
-    'q': "banha" ,# You can also use 'q' parameter for city name
-    "units":"metric"
-}
-@app.route("/")
+
+@app.route("/",methods=["GET","POST"])
 def start():
+    city_name = "banha"
+    if request.method=="POST":
+        print("i am in post mode ")
+        city_name=request.form.get("city_name")
+        print(city_name)
+    params = {
+        'appid': API_KEY,
+        'q': city_name,  # You can also use 'q' parameter for city name
+        "units": "metric"
+    }
 
     response=requests.get(END_POINT,params)
     temp=response.json()
